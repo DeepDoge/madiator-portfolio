ae753df3d4540a5eedc4be6170601bb8e2344fd2
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-01-05 09:47:41 +0000

    🍤 handled show not found

--- src/routes/api/get_show.ts
+++ src/routes/api/get_show.ts
@@ -1,6 +1,7 @@
 import type { Locals } from "$lib/types"
 import type { RequestHandler } from "@sveltejs/kit"
 import type { ServerRequest } from "@sveltejs/kit/types/hooks"
+import { existsSync } from "fs"
 import { mkdir, readdir } from "fs/promises"
 import path from 'path'
 import { api } from "./_api"
@@ -22,11 +23,11 @@ export const get: RequestHandler<Locals> = async (req: ServerRequest) =>
 {
     return await api(async () =>
     {
-
         await mkdir(showsDirname, { recursive: true })
         const name = req.url.searchParams.get('name')
         const showImagesDirname = path.join(showsDirname, name, 'images')
         const showImagesDirnamePublic = path.join(showsDirnamePublic, name, 'images')
+        if (!existsSync(showImagesDirname)) return []
         const showFiles = Object.fromEntries((await Promise.all((await readdir(showImagesDirname, { encoding: 'utf-8', withFileTypes: true }))
             .filter((file) => file.isDirectory())
             .map(async (imageDir) =>
